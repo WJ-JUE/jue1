1c18dc5521d1d4b56a4d7735a4e5b025a7d0ef6a
jue <842745074@qq.com>
2023-01-18 16:20:40 +0800

    第一次提交

---
+++ wj/test1.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+
+# decode格式
+
+url = 'http://www.baidu.com'
+resp = urlopen(url)
+t = resp.read().decode('utf-8')
+print(t)
+
+with open("1.html",'w',encoding='utf-8') as f:
+    f.write(t)
+print("over!")
